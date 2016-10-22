ac2e14b322b0a0b3a516f54021e5e72a6b32ca27
Jørgen Halvorsen <jorgen-halvorsen@hotmail.com>
2016-10-22 15:58:05 +0200

    Kan nå filtrere ut elementer basert på jingler, musikk, stikk og lydsaker.

--- src/pages/modules/dayShows.js
+++ src/pages/modules/dayShows.js
@@ -22,7 +22,7 @@ module.exports = React.createClass({
 		var startTime = new Date(this.props.show.starttime);
 		var endtime = new Date(this.props.show.endtime);
 		return (
-			<tr onClick={this.chooseThis} style={{cursor: "pointer"}}>
+			<tr onClick={this.chooseThis} style={{cursor: "pointer"}} className={this.props.selected}>
 				<td>{this.props.show.title}</td>
 				<td>{this.getNiceNumber(startTime.getHours())}:{this.getNiceNumber(startTime.getMinutes())} - {this.getNiceNumber(endtime.getHours())}:{this.getNiceNumber(endtime.getMinutes())}</td>
 			</tr>
--- src/pages/showTemplate.js
+++ src/pages/showTemplate.js
@@ -15,13 +15,34 @@ module.exports = React.createClass({
 			url: "",
 			elements: [],
 			show: {},
-			showSkipped: true
+			showSkipped: false,
+			showAudio: false,
+			showMusic: true,
+			showJingles: false
 		}
 	},
 
 	toggelSkipped: function() {
 		this.setState({
 			showSkipped: !this.state.showSkipped
+		});
+	},
+
+	toggleShowAudio: function() {
+		this.setState({
+			showAudio: !this.state.showAudio
+		});
+	},
+
+	toggleMusic: function() {
+		this.setState({
+			showMusic: !this.state.showMusic
+		});
+	},
+
+	toggleJingles: function() {
+		this.setState({
+			showJingles: !this.state.showJingles
 		});
 	},
 
@@ -57,7 +78,8 @@ module.exports = React.createClass({
 		console.log("Update elements kis.");
 		// console.log(this.state.url);
 		this.setState({
-			show: id
+			show: id,
+			elements: [{fake_id:"Lolas", title:"Laster inn sendeskjemaet"}]
 		});
 		var startDate = new Date(id.starttime);
 		var endDate = new Date(id.endtime);
@@ -66,32 +88,77 @@ module.exports = React.createClass({
 			success: function(res) {
 				var arr = [];
 				var counter = 0;
+				var foundLast = false
 				res.elements.map(function(item) {
-					for (var a in arr) {
-						if (arr[a].artist === item.artist && arr[a].title === item.title)
-							return;
+					if (foundLast)
+						return;
+
+					if (endDate <= new Date(item.actual_stop)){
+						foundLast = true;
 					}
+					// for (var a in arr) {
+					// 	// console.log("ARR: artist " + arr[a].artist + ", title: " + arr[a].title);
+					// 	// console.log("Item: artist " + item.artist + ", title: " + item.title);
+					// 	if (item.artist === "Hawkon") {
+					// 		console.log("Denne skal være true:");
+					// 		console.log("Arr: " + arr[a].artist + "   " + arr[a].artist.length);
+					// 		console.log("Item: " + item.artist + "  " + item.artist.length);
+					// 		console.log(arr[a].artist === item.artist && arr[a].title === item.title);
+					// 	}
+					// 	if (arr[a].artist === item.artist && arr[a].title === item.title)
+					// 		return;
+					// 	if (arr[a].id === item.id) {
+					// 		item.id = item.id + "errorId";
+					// 		break;
+					// 	}
+					// }
 					if (item.sendstate === "Skipped" && (item.class === "Live" || item.class === "None")) {
 						item.fake_id = counter;
 						arr.push(item);
 						counter++;
 					}
-					else if (item.from_jinglebank || item.media_type !== "Audio" || item.class !== "Music" 
-						|| startDate < item.actual_start || endDate < item.actual_stop) {
-
+					else if (item.from_jinglebank || item.media_type !== "Audio" 
+						|| (item.class !== "Music" && item.class !== "Audio" && item.class !== "Promotion") 
+						|| startDate > new Date(item.actual_start)) {
+						//do nothing
 					} else {
 						item.fake_id = counter;
 						arr.push(item);
 						counter++;
+						// console.log(item);
 					}
 				}.bind(this));
-				if (!arr.length) {
+
+				var newArr = []
+
+				for (var i in arr) {
+					var found = false;
+					for (var j in newArr) {
+						if (arr[i].artist === newArr[j].artist && newArr[j].title === arr[i].title && arr[i].class === "Music" 
+							|| (arr[i].class === "None" && arr[i].actual_start === newArr[j].actual_start)
+							|| ((arr[i].class === "Audio" || arr[i].class === "Promotion") && arr[i].title === newArr[j].title)
+							){
+							found = true;
+							break;
+						}
+					}
+					if (!found) {
+						newArr.push(arr[i]);
+					}
+				}
+
+				if (this.state.show.id !== res.metadata.id) {
+					console.log("The program was changed, aborting.");
+					return;
+				}
+
+				if (!newArr.length) {
 					this.setState({
-						elements: [{id:"mariuserenkuk", title:"Ingen elementer ble funnet."}]
+						elements: [{fake_id:"mariuserenkuk", title:"Ingen elementer ble funnet."}]
 					});
 				} else {
 					this.setState({
-						elements: arr
+						elements: newArr
 					});
 				}
 			}.bind(this),
@@ -121,27 +188,63 @@ module.exports = React.createClass({
 	},
 
 	render: function() {
+		var musicToggleRow = this.state.showMusic ? "song-row" : "";
+		var skippedToggleRow = this.state.showSkipped ? "skipped-row": "";
+		var audioToggleRow = this.state.showAudio ? "audio-row": "";
+		var jingleToggleRow = this.state.showJingles ? "jingle-row": "";
 		return (
 			<div>
 				<div className="row">
 					<div className="col-md-4">
 						<h3>Dabtext Shows</h3>
 						<DateFields callback={this.updateDate} />
-						<div>
-							Vis skippede elementer <input type="checkbox" checked={this.state.showSkipped} onChange={this.toggelSkipped} />
-						</div>
-						<h4>Programmer denne dagen</h4>
+
+						<h4>Programmer den {this.state.day}.{this.state.month}.{this.state.year}</h4>
 						<table className="table">
 							<tbody>
 								{
 									this.state.shows.map(function(show) {
+										var selected = this.state.show.id === show.id ? "chosen-program-row": "";
 										return (
-											<DayShow key={show.id} callback={this.updateElements} show={show} />
+											<DayShow selected={selected} key={show.id} callback={this.updateElements} show={show} />
 										);
 									}.bind(this))
 								}
 							</tbody>
 						</table>
+						<div>
+							<h4>Filter</h4>
+							<table className="table">
+								<tbody>
+									<tr className={musicToggleRow} onClick={this.toggleMusic}>
+										<td>Musikk</td>
+										<td>
+											<input id="music_elements" type="checkbox" checked={this.state.showMusic} onChange={this.toggleMusic} />
+										</td>
+									</tr>
+									<tr className={audioToggleRow} onClick={this.toggleShowAudio}>
+										<td>Lydsaker</td>
+										<td>
+											<input id="audio_elements" type="checkbox" checked={this.state.showAudio} onChange={this.toggleShowAudio} />
+										</td>
+									</tr>
+									<tr className={skippedToggleRow} onClick={this.toggelSkipped}>
+										<td>
+											Stikkelementer
+										</td>
+										<td>
+											<input id="skipped_elements" type="checkbox" checked={this.state.showSkipped} onChange={this.toggelSkipped} />
+										</td>
+									</tr>
+									<tr className={jingleToggleRow} onClick={this.toggleJingles}>
+										<td>Jingler</td>
+										<td>
+											<input id="jingle_elements" type="checkbox" checked={this.state.showJingles} onChange={this.toggleJingles} />
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
 					</div>
 					<div className="col-md-8">
 						<h3>{this.state.show.title}: {this.getNiceTime(this.state.show.starttime)} - {this.getNiceTime(this.state.show.endtime)}</h3>
@@ -149,11 +252,26 @@ module.exports = React.createClass({
 							<tbody>
 								{
 									this.state.elements.map(function(elem) {
-										if (!this.state.showSkipped && elem.sendstate === "Skipped") {
-											return null;
-										} 
+										var typeClass = "song-row";
+										if (elem.sendstate === "Skipped") {
+											if (!this.state.showSkipped)
+												return null;
+											typeClass = "skipped-row";
+										} else if (elem.class === "Audio") {
+											if (!this.state.showAudio)
+												return null;
+											typeClass = "audio-row";
+										} else if (elem.class === "Music") {
+											if (!this.state.showMusic)
+												return null;
+										} else if (elem.class === "Promotion") {
+											if (!this.state.showJingles) 
+												return null;
+											typeClass = "jingle-row";
+										}
+										// console.log(elem.id);
 										return (
-											<tr key={elem.id}>
+											<tr key={elem.fake_id} className={typeClass}>
 												<td>{elem.title}</td>
 												<td>{elem.artist}</td>
 												<td>{elem.album}</td>
