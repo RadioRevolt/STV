6af3932ca759f78ea0482d789e020db53658cff7
Jørgen Halvorsen <jorgen-halvorsen@hotmail.com>
2016-10-30 22:20:52 +0100

    Flyttet noe av koden over til JS6. Erstattet jQuery med Github fetch. Lagt inn navn på dager og måneder for enklere browsing

--- src/pages/modules/dateFields.js
+++ src/pages/modules/dateFields.js
@@ -1,67 +1,93 @@
 
-var React = require("react");
+const React = require("react");
 
-var DateField = React.createClass({
-	getInitialState: function() {
-		var days = [];
-		for (var i = 1; i < 32; i++) {
+
+class DateField extends React.Component {
+	constructor(props) {
+		super(props);
+		const days = [];
+		for (let i = 1; i < 32; i++) {
 			days.push(i);
 		}
-		var months = [];
-		for (var i = 1; i <= 12; i++) {
+		const months = [];
+		for (let i = 1; i <= 12; i++) {
 			months.push(i);
 		}
-		var years = [];
-		for (var i = 2010; i <= new Date().getFullYear(); i++) {
+		const years = [];
+		for (let i = 2010; i <= new Date().getFullYear(); i++) {
 			years.push(i);
 		}
 		// this.props.callback(days[0], months[0], years[0]);
-		var today = new Date();
-		return {
+		const today = new Date();
+		this.state = {
 			days: days,
 			months: months,
 			years: years,
 			day: days[today.getDate() - 1],
 			month: months[today.getMonth()],
-			year: years[years.length - 1]
-		}
-	},
+			year: years[years.length - 1],
+			source: "studio"
+		};
 
-	updateParent: function() {
+		this.monthNames = ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"];
+		this.dayNames = ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"];
+	}
+
+  	changeSource(e)  {
+		this.setState({source: e.target.value});
+		this.props.doChangeSource(e.target.value);
+	}
+
+	updateParent() {
 		this.props.callback(this.state.day, this.state.month, this.state.year);
-	},
+	}
 
-	changeYear: function(e) {
+	changeYear(e) {
 		this.setState({year: e.target.value});
 		// this.updateParent();
 		this.props.callback(this.state.day, this.state.month, e.target.value);
-	},
+	}
 
-	changeMonth: function(e) {
+	changeMonth(e) {
 		this.setState({month: e.target.value});
 		// this.updateParent();
 		this.props.callback(this.state.day, e.target.value, this.state.year);
-	},
+	}
 
-	changeDay: function(e) {
+	changeDay(e) {
 		this.setState({day: e.target.value});
 		// this.updateParent();
 		this.props.callback(e.target.value, this.state.month, this.state.year);
-	},
+	}
 
-	componentDidMount: function() {
+	componentDidMount() {
 		this.updateParent();
-	}, 
+	}
 
-	render: function() {
+	getDayName(day) {
+		const date = new Date(this.state.year, this.state.month - 1, day);
+		return this.dayNames[date.getDay()];
+	}
+
+	render() {
 		return (
 			<div>
 				<table className="table">
 					<thead>
 						<tr>
+							<td>Kilde</td>
+							<td>
+								<select className="form-control" value={this.state.source} onChange={this.changeSource.bind(this)}>
+									<option value="studio" key="studio">Studio</option>
+									<option value="autoavvikler" key="autoavvikler">Autoavvikler</option>
+									<option value="teknikerrom" key="teknikerrom">Teknikerrom</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
 							<td>År</td>
 							<td>
-								<select className="form-control" value={this.state.year} onChange={this.changeYear}>
+								<select className="form-control" value={this.state.year} onChange={this.changeYear.bind(this)}>
 									{
 										this.state.years.map(function(year) {
 											return (
@@ -75,11 +101,12 @@ var DateField = React.createClass({
 						<tr>
 							<td>Måned</td>
 							<td>
-								<select className="form-control" value={this.state.month} onChange={this.changeMonth}>
+								<select className="form-control" value={this.state.month} onChange={this.changeMonth.bind(this)}>
 									{
 										this.state.months.map(function(month) {
-											return (<option value={month} key={month}>{month}</option>);
-										})
+											const name = this.monthNames[month - 1];
+											return (<option value={month} key={month}>{name}</option>);
+										}.bind(this))
 									}
 								</select>
 							</td>
@@ -87,11 +114,19 @@ var DateField = React.createClass({
 						<tr>
 							<td>Dag</td>
 							<td>
-								<select className="form-control" value={this.state.day} onChange={this.changeDay}>
+								<select className="form-control" value={this.state.day} onChange={this.changeDay.bind(this)}>
 									{
 										this.state.days.map(function(day) {
-											return (<option value={day} key={day}>{day}</option>);
-										})
+											const name = this.getDayName(day);
+											// if (day > 28) {
+											// 	const month = this.state.month;
+											// 	if (month === 2)
+											// 		return null;
+											// 	if (month === 4 || month === 6 || month === 9 || month === 11)
+											// 		return null;
+											// }
+											return (<option value={day} key={day}>{day} - {name}</option>);
+										}.bind(this))
 									}
 								</select>
 							</td>
@@ -101,27 +136,6 @@ var DateField = React.createClass({
 			</div>
 		);
 	}
-});
+}
 
-module.exports.first = React.createClass({
-	render: function() {
-		return (
-			<tr>
-				<td>Dato for produseringen</td>
-				<td><DateField callback={this.props.callback} /></td>
-			</tr>
-		);
-	}
-});
-
-
-module.exports.second = React.createClass({
-	render: function() {
-		// console.log("nå har dato oppdatert seg.");
-		return (
-			<div>
-				<DateField callback={this.props.callback} />
-			</div>
-		)
-	}
-});
+module.exports = DateField;
--- src/pages/showTemplate.js
+++ src/pages/showTemplate.js
@@ -1,12 +1,12 @@
 
-var React = require("react");
-var DateFields = require("./modules/dateFields.js").second;
-var $ = require("jquery");
-var DayShow = require("./modules/dayShows.js");
+const React = require("react");
+const DateFields = require("./modules/dateFields.js");
+// const $ = require("jquery");
+const DayShow = require("./modules/dayShows.js");
+
 
 module.exports = React.createClass({
 	getInitialState: function() {
-		var today = new Date();
 		return {
 			day: 0,
 			month: 0,
@@ -18,7 +18,8 @@ module.exports = React.createClass({
 			showSkipped: false,
 			showAudio: false,
 			showMusic: true,
-			showJingles: false
+			showJingles: false,
+			source: "studio"
 		}
 	},
 
@@ -46,6 +47,35 @@ module.exports = React.createClass({
 		});
 	},
 
+	changeSource: function(source) {
+		console.log("Souce updated.");
+		this.setState({
+			source: source
+		});
+		this.receiveShows(this.state.day, this.state.month, this.state.year, source);
+	},
+
+	receiveShows: function(day, month, year, source) {
+		const url = "http://pappagorg.radiorevolt.no/v1/sendinger/dato/" + year + "/" + month + "/" + day + "/" + source + "/";
+		this.setState({
+			url: url,
+			shows: [],
+			show: {},
+			elements: []
+		});
+
+		fetch(url).then(function(response) {
+			return response.json();
+		}).then(function(json) {
+			this.setState({
+				shows: json
+			});
+		}.bind(this)).catch(err => {
+			console.log("An error kis.");
+			console.log(err);
+		});
+	},
+
 	updateDate: function(day, month, year) {
 		console.log("Date updated kis: " + year + ":" + month + ":" + day);
 		this.setState({
@@ -53,158 +83,125 @@ module.exports = React.createClass({
 			month: month,
 			year: year
 		});
-
-		var url = "http://pappagorg.radiorevolt.no/v1/sendinger/dato/" + year + "/" + month + "/" + day + "/studio/";
-		this.setState({
-			url: url
-		});
-		$.ajax({
-			url: url,
-			success: function(res) {
-				this.setState({
-					shows: res,
-					elements: [],
-					show: {}
-				});
-			}.bind(this),
-			error: function(err) {
-				console.log("Nå skjedde det en error kis.");
-				console.log(err);
-			}
-		});
+		this.receiveShows(day, month, year, this.state.source);
 	},
 
 	updateElements: function(id) {
 		console.log("Update elements kis.");
-		// console.log(this.state.url);
+
 		this.setState({
 			show: id,
 			elements: [{fake_id:"Lolas", title:"Laster inn sendeskjemaet"}]
 		});
-		var startDate = new Date(id.starttime);
-		var endDate = new Date(id.endtime);
-		$.ajax({
-			url: this.state.url + "/" + id.id,
-			success: function(res) {
-				var arr = [];
-				var counter = 0;
-				var foundLast = false
-				res.elements.map(function(item) {
-					if (foundLast)
-						return;
 
-					if (endDate <= new Date(item.actual_stop)){
-						foundLast = true;
-					}
-					// for (var a in arr) {
-					// 	// console.log("ARR: artist " + arr[a].artist + ", title: " + arr[a].title);
-					// 	// console.log("Item: artist " + item.artist + ", title: " + item.title);
-					// 	if (item.artist === "Hawkon") {
-					// 		console.log("Denne skal være true:");
-					// 		console.log("Arr: " + arr[a].artist + "   " + arr[a].artist.length);
-					// 		console.log("Item: " + item.artist + "  " + item.artist.length);
-					// 		console.log(arr[a].artist === item.artist && arr[a].title === item.title);
-					// 	}
-					// 	if (arr[a].artist === item.artist && arr[a].title === item.title)
-					// 		return;
-					// 	if (arr[a].id === item.id) {
-					// 		item.id = item.id + "errorId";
-					// 		break;
-					// 	}
-					// }
-					if (item.sendstate === "Skipped" && (item.class === "Live" || item.class === "None")) {
-						item.fake_id = counter;
-						arr.push(item);
-						counter++;
-					}
-					else if (item.from_jinglebank || item.media_type !== "Audio" 
-						|| (item.class !== "Music" && item.class !== "Audio" && item.class !== "Promotion") 
-						|| startDate > new Date(item.actual_start)) {
-						//do nothing
-					} else {
-						item.fake_id = counter;
-						arr.push(item);
-						counter++;
-						// console.log(item);
-					}
-				}.bind(this));
+		const startDate = new Date(id.starttime);
+		const endDate = new Date(id.endtime);
+		fetch(this.state.url + "/" + id.id).then(res => {
+			return res.json();
+		}).then(data => {
+			const arr = [];
+			let counter = 0;
+			let foundLast = false;
 
-				var newArr = []
-
-				for (var i in arr) {
-					var found = false;
-					for (var j in newArr) {
-						if (arr[i].artist === newArr[j].artist && newArr[j].title === arr[i].title && arr[i].class === "Music" 
-							|| (arr[i].class === "None" && arr[i].actual_start === newArr[j].actual_start)
-							|| ((arr[i].class === "Audio" || arr[i].class === "Promotion") && arr[i].title === newArr[j].title)
-							){
-							found = true;
-							break;
-						}
-					}
-					if (!found) {
-						newArr.push(arr[i]);
-					}
-				}
-
-				if (this.state.show.id !== res.metadata.id) {
-					console.log("The program was changed, aborting.");
+			data.elements.map(item => {
+				if (foundLast)
 					return;
+
+				if (endDate <= new Date(item.actual_stop)){
+					foundLast = true;
 				}
 
-				if (!newArr.length) {
-					this.setState({
-						elements: [{fake_id:"mariuserenkuk", title:"Ingen elementer ble funnet."}]
-					});
-				} else {
-					this.setState({
-						elements: newArr
-					});
+				if (item.sendstate === "Skipped" && (item.class === "Live" || item.class === "None")) {
+					item.fake_id = counter;
+					arr.push(item);
+					counter++;
 				}
-			}.bind(this),
-			error: function(err) {
-				console.log(err);
-				console.log("Enda en error kis.");
+				else if (item.from_jinglebank || item.media_type !== "Audio" 
+					|| (item.class !== "Music" && item.class !== "Audio" && item.class !== "Promotion") 
+					|| startDate > new Date(item.actual_start)) {
+					//do nothing
+				} else {
+					item.fake_id = counter;
+					arr.push(item);
+					counter++;
+					// console.log(item);
+				}
+			});
+
+			const newArr = [];
+
+			for (let i in arr) {
+				let found = false;
+				for (let j in newArr) {
+					if (arr[i].artist === newArr[j].artist && newArr[j].title === arr[i].title && arr[i].class === "Music" 
+						|| (arr[i].class === "None" && arr[i].actual_start === newArr[j].actual_start)
+						|| ((arr[i].class === "Audio" || arr[i].class === "Promotion") && arr[i].title === newArr[j].title)
+						){
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					newArr.push(arr[i]);
+				}
 			}
+
+			if (this.state.show.id !== data.metadata.id) {
+				console.log("The program was changed, aborting.");
+				return;
+			}
+
+			if (!newArr.length) {
+				this.setState({
+					elements: [{fake_id:"mariuserenkuk", title:"Ingen elementer ble funnet."}]
+				});
+			} else {
+				this.setState({
+					elements: newArr
+				});
+			}
+
+		}).catch(err => {
+			console.log("Error!");
+			console.log(err);
 		});
-		// console.log(id);
 	},
 
 	getNiceTime: function(date, secs) {
 		if (!date)
 			return;
 		date = new Date(date);
-		var hours = date.getHours(), minutes = date.getMinutes();
+		let hours = date.getHours(), minutes = date.getMinutes();
 		if (hours < 10)
 			hours = "0" + hours;
 		if (minutes < 10) 
 			minutes = "0" + minutes;
 		if (!secs)
 			return hours + ":" + minutes;
-		var seconds = date.getSeconds();
+		let seconds = date.getSeconds();
 		if (seconds < 10)
 			seconds = "0" + seconds;
 		return hours + ":" + minutes + ":" + seconds
 	},
 
 	render: function() {
-		var musicToggleRow = this.state.showMusic ? "song-row" : "";
-		var skippedToggleRow = this.state.showSkipped ? "skipped-row": "";
-		var audioToggleRow = this.state.showAudio ? "audio-row": "";
-		var jingleToggleRow = this.state.showJingles ? "jingle-row": "";
+		const musicToggleRow = "show-row " + (this.state.showMusic ? "song-row" : "");
+		const skippedToggleRow = "show-row " + (this.state.showSkipped ? "skipped-row": "");
+		const audioToggleRow = "show-row " + (this.state.showAudio ? "audio-row": "");
+		const jingleToggleRow = "show-row " + (this.state.showJingles ? "jingle-row": "");
 		return (
 			<div>
 				<div className="row">
 					<div className="col-md-4">
 						<h3>Dabtext Shows</h3>
-						<DateFields callback={this.updateDate} />
+						<DateFields callback={this.updateDate} doChangeSource={this.changeSource} />
 
 						<h4>Programmer den {this.state.day}.{this.state.month}.{this.state.year}</h4>
 						<table className="table">
 							<tbody>
 								{
 									this.state.shows.map(function(show) {
-										var selected = this.state.show.id === show.id ? "chosen-program-row": "";
+										const selected = this.state.show.id === show.id ? "chosen-program-row": "";
 										return (
 											<DayShow selected={selected} key={show.id} callback={this.updateElements} show={show} />
 										);
@@ -252,7 +249,7 @@ module.exports = React.createClass({
 							<tbody>
 								{
 									this.state.elements.map(function(elem) {
-										var typeClass = "song-row";
+										let typeClass = "song-row";
 										if (elem.sendstate === "Skipped") {
 											if (!this.state.showSkipped)
 												return null;
